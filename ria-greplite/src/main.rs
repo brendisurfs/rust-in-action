585b0af116d2569731106c8ccd14ac61f8cc0e12
Brendan Prednis <brendan.prednis@protonmail.com>
2022-01-27 21:59:34 -0800

    added grep-lite.

---
+++ ria-greplite/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("grep lite");
+
+    let search_term = "picture";
+    let quote = "\
+     Every face, every shop, bedroom window, public-house, and
+     dark square is a picture feverishly turned--in search of what?
+     It is the same with books.
+     What do we seek through millions of pages?";
+
+    for (idx, line) in quote.lines().enumerate() {
+        if line.contains(search_term) {
+            println!("\nline {idx}: {line}");
+        }
+    }
+}
